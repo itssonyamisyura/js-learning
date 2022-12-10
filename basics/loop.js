ff100a51a170f33fe05566c8c311c50c05af2c47
Sonya <itssonyamisyura@gmail.com>
2022-12-10 16:33:37 +0100

    loop

---
+++ basics/loop.js
@@ -0,0 +1,75 @@
+"use strict";
+
+// let num = 50;
+
+// while ( num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//   console.log(num);
+//   num++;
+// }
+// while (num < 55);
+
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6) {
+//       // break;
+//       continue;
+//     }
+//     console.log(i);
+// }
+
+for (let i = 0; i < 3; i++) {
+    console.log(i);
+    for (let j = 0; j < 3; j++) {
+      console.log(j);
+  }
+}
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+let result = "";
+const length = 7;
+
+for (let i = 1; i < length; i++) {
+
+  for (let j = 0; j < i; j++) {
+    result += "*";
+  }
+
+  result += "\n";
+}
+
+console.log(result);
+
+
+  let i = 5;
+  while ( i <=10 ) {
+      console.log(i);
+      i++;
+  }
+
+  for (let i = 20; i >= 10; i--) {
+    console.log (i);
+    if (i === 13) {
+        break;
+    }
+}
+for (let i = 2; i <= 10; i++) {
+  if ( i % 2 ) {}
+}
+
+console.log(11 % 3);
+
+for (let i = 2; i <= 10; i++) {
+  if ( i % 2 === 0) {
+      console.log(i);
+  } 
+}
