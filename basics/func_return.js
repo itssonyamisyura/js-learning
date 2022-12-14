f15b710716352d03e1f906d6a9865584d4a9a3b9
Sonya <itssonyamisyura@gmail.com>
2022-12-14 19:29:58 +0100

    userData

--- basics/func_return.js
+++ basics/func_return.js
@@ -28,3 +28,45 @@ function doNothing() {};
 console.log(doNothing() === undefined);
 // func всегда нам что-то возвращает, даже если мы не видим return
 // undefined
+
+
+
+const userData = {
+  name: "rostik",
+  age: 20
+};
+console.log(`hello ${userData.name} im ${userData.age}`);
+console.log("hello " + userData.name + " i am " + userData.age);
+
+
+
+
+
+function sayHello(name) {
+  // console.log(name);
+  // return "Привет" + name;
+  return `Привет ${name}`;
+}
+sayHello("Sonya");
+console.log(sayHello("Sonya"));
+
+const myName = sayHello("Sonya");
+console.log(myName);
+
+
+// 2) Создайте функцию, которая принимает в себя 1 аргумент в виде целого числа и возвращает массив из трех чисел: одно на 1 меньше, сам аргумент, и число на 1 больше.
+
+
+function returnNeighboringNumbers(num) {
+    let array = [];
+    // array[0] = num - 1;
+    // array[1] = num;
+    // array[2] = num + 1;
+    let a = num - 1;
+    for(let i = 0; i<3; i++) {
+      array[i] = a;
+      a++;
+    }
+    console.log(array);
+}
+returnNeighboringNumbers(4);
